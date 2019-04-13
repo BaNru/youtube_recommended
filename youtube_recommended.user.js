0952ccd69d79b2f11b1263a0eaf3f6d9db5bbbcc
Andrey <admin@g63.ru>
2019-04-13 12:58:48 +0400

    Версия 3
    Запоздалое обновление под очередное изменение Ютуба.
    Добавлена кнопка отправки видео в спам.
    Добавлена кнопка перезагрузки кнопок скрипта.

--- youtube_recommended.user.js
+++ youtube_recommended.user.js
@@ -3,7 +3,7 @@
 // @namespace   youtube, recommended
 // @description Удаление из рекомендуемого в пару кликов
 // @include     https://www.youtube.com/*
-// @version     2.1
+// @version     3
 // @grant       none
 // @author      BaNru
 // @license     GNU GPL v3
@@ -14,14 +14,14 @@ function removeall(list){
 	[].forEach.call(list, function(list_el) {
 		//if(list_el.style.visibility == "visible"){
 			//list_el.querySelector('.yt-ui-menu-item.yt-uix-menu-close-on-select.dismiss-menu-choice').click();
-			//removthisevent(list_el);
+			//removethisevent(list_el);
 		//}
 		if (document.querySelector('ytd-popup-container iron-dropdown') && document.querySelector('ytd-popup-container iron-dropdown').style.display !== "none"){
 			document.querySelector('ytd-popup-container iron-dropdown').style.display = "none";
 		}
 		console.log(list_el.querySelector('button#button'))
 		list_el.querySelector('button#button').click();
-		removthisevent(list_el);
+		removethisevent(list_el);
 	});
 }
 */
@@ -34,13 +34,14 @@ function removethis(el){
 	}
 	var thisli = this.closest('ytd-grid-video-renderer,ytd-compact-video-renderer');
 	thisli.querySelector('button#button').click();
-	removthisevent(thisli);
+	removethisevent(thisli);
 }
-function removthisevent(thisli){
+/* Удалить */
+function removethisevent(thisli){
 	// Проверяем открытый попап
 	if (document.querySelector('ytd-popup-container iron-dropdown') && document.querySelector('ytd-popup-container iron-dropdown').style.display !== "none"){
 			// Нажимаем "Скрыть"
-			document.querySelector('ytd-popup-container ytd-menu-service-item-renderer').click();
+			document.querySelectorAll('ytd-popup-container ytd-menu-service-item-renderer')[2].click();
 			// Нажимаем "почему нам это не интересно"
 			// Включаем "я не тормоз, подождите" для нового тормозного интерфейса
 			setTimeout(function(){
@@ -65,7 +66,52 @@ function removthisevent(thisli){
 	// Иначе перезапускаем функцию после паузы
 	else {
 		setTimeout(function(){
-			removthisevent(thisli);
+			removethisevent(thisli);
+		},500); // Время задержки
+	}
+}
+/* Удаляем элемент */
+function spamthis(el){
+  console.log(el);
+	// Хак на скрытие попапа, если вдруг пользователь открыл его ранее
+	if (document.querySelector('ytd-popup-container iron-dropdown') && document.querySelector('ytd-popup-container iron-dropdown').style.display !== "none"){
+		document.querySelector('ytd-popup-container iron-dropdown').style.display = "none";
+	}
+	var thisli = this.closest('ytd-grid-video-renderer,ytd-compact-video-renderer');
+	thisli.querySelector('button#button').click();
+	spamthisevent(thisli);
+}
+/* В спам */
+function spamthisevent(thisli){
+	// Проверяем открытый попап
+	if (document.querySelector('ytd-popup-container iron-dropdown') && document.querySelector('ytd-popup-container iron-dropdown').style.display !== "none"){
+			// Нажимаем "Пожаловаться"
+			document.querySelectorAll('ytd-popup-container ytd-menu-service-item-renderer')[3].click();
+			// Нажимаем почему дерьмо надо заблокировать
+			// Включаем "я не тормоз, подождите" для нового тормозного интерфейса
+			setTimeout(function(){
+				// Ожидаем попап
+				setTimeout(function(){
+					var popup = document.querySelector('yt-report-form-modal-renderer'),
+						inputs = popup.querySelectorAll('paper-radio-button');
+						// Чекаем нужный пункт (только один пункт!)
+            // inputs[0].querySelector('paper-checkbox').click(); // Материалы сексуального характера
+            // inputs[1].querySelector('paper-checkbox').click(); // Жестокие или отталкивающие сцены
+            // inputs[2].querySelector('paper-checkbox').click(); // Оскорбления или проявления нетерпимости
+            // inputs[3].querySelector('paper-checkbox').click(); // Вредные или опасные действия
+            inputs[4].click(); // Спам или введение в заблуждение
+						// Отправляем форму
+						setTimeout(function(){
+							popup.querySelector('#submit-button').click();
+							thisli.remove();
+						},200);
+				},200);
+			},500);
+	}
+  // Иначе перезапускаем функцию после паузы
+	else {
+		setTimeout(function(){
+			spamthisevent(thisli);
 		},500); // Время задержки
 	}
 }
@@ -75,7 +121,7 @@ function addBTN(parentel,el){
   if(el){
 		var list = el.closest('#dismissable').querySelectorAll('ytd-grid-video-renderer');
 	}else{
-	var list = parentel.querySelectorAll('ytd-compact-video-renderer');
+		var list = parentel.querySelectorAll('ytd-compact-video-renderer');
   }
 	/*
 	delall.onclick = function(){
@@ -90,11 +136,26 @@ function addBTN(parentel,el){
 			remthis.className = "remthis";
 			list_el.insertBefore(remthis, list_el.children[0]);
 		}
+    if(!list_el.querySelector('.spamthis')){
+      var spamthisBTN = document.createElement('div');
+			spamthisBTN.textContent = 'Спам';
+			spamthisBTN.onclick = spamthis;
+			spamthisBTN.className = "spamthis";
+			list_el.insertBefore(spamthisBTN, list_el.children[0]);
+    }
 	});
 };
 
 /* Стили */
-document.querySelector('body').insertAdjacentHTML('beforeend', '<style>.delall,.remthis{background-color:#008bec;color:#fff;cursor:pointer;font-size:12px;padding:2px 7px;z-index:2000;}.delall{margin:1px;float:right;}.remthis{margin:5px;position:absolute;display:block;}.delall:hover,.remthis:hover{background-color:#cb4437}</style>');
+document.querySelector('body').insertAdjacentHTML('beforeend', `<style>
+  .delall,.remthis,.spamthis{background-color:#008bec;color:#fff;cursor:pointer;font-size:12px;padding:2px 7px;z-index:2000;}
+  .delall{margin:1px;float:right;}
+  .remthis,.spamthis{margin:5px;position:absolute;display:block;}
+  .delall:hover,.remthis:hover{background-color:#cb4437}
+	.spamthis{left:65px;background-color:#cb4437;}
+  .spamthis:hover{background-color:#262626;}
+	.reloadYR{position:fixed;right:0;bottom:0;width:32px;height:32px;background-color:#cb4437;z-index:999999;font-size:24px;text-align:center;line-height:32px;color:#fff;border-radius:16px 0 0 0;padding:2px 0 0 6px;cursor:pointer;}
+</style>`);
 
 
 // Запускаем скрипт на главной
@@ -128,20 +189,29 @@ if(document.location.pathname.match(/^\/watch/)){
 	var waitRecomendI = 0;
 	// Включаем "я не тормоз, подождите" для тормозного интерфейса Ютуба
 	let waitRecomend = setInterval(function(){
-	waitRecomendI++;
-	console.log(waitRecomendI);
-	if(waitRecomendI == 20){
-		console.log('STOP Youtube recommended');
-		clearInterval(waitRecomend);
-	}
-	var parentel = document.querySelector('#related #items');
-	if(parentel){
-		clearInterval(waitRecomend);
-		// Включаем ещё раз "я не тормоз, подождите" для тормозного интерфейса Ютуба
-		setTimeout(function(){
-			addBTN(parentel);
-		// Увеличить число в 2-5 раз, если комп или интернет слишком медленный и не у всех видео появляется кнопка "удалить". 1000 = 1 сек
-		},1000)
-	}
+    waitRecomendI++;
+    console.log(waitRecomendI);
+    if(waitRecomendI == 20){
+      console.log('STOP Youtube recommended');
+      clearInterval(waitRecomend);
+    }
+    var parentel = document.querySelector('#related #items');
+    if(parentel){
+      clearInterval(waitRecomend);
+      // Включаем ещё раз "я не тормоз, подождите" для тормозного интерфейса Ютуба
+      setTimeout(function(){
+        addBTN(parentel);
+      // Увеличить число в 2-5 раз, если комп или интернет слишком медленный и не у всех видео появляется кнопка "удалить". 1000 = 1 сек
+      },1000)
+    }
   }, 500);
+
+  var reloadBTN = document.createElement('span');
+  reloadBTN.textContent = '⟳';
+  reloadBTN.onclick = function(){
+    addBTN(document.querySelector('#related #items'))
+  };
+  reloadBTN.className = "reloadYR";
+  var head = document.body;
+  head.insertBefore(reloadBTN, head.children[1]);
 }
